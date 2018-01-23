0a42e492410752ee7c5112fda29ff62c9e44fb06
unknown <Chengguo_Li@NC.LAMSOON.COM>
2018-01-23 15:20:06 +0800

    After Inheritance

--- Migrations/SchoolContextModelSnapshot.cs
+++ Migrations/SchoolContextModelSnapshot.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.EntityFrameworkCore.Storage.Internal;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
 using System;
 
 namespace ContosoUniversity.Migrations
@@ -98,27 +99,6 @@ namespace ContosoUniversity.Migrations
                     b.ToTable("Enrollment");
                 });
 
-            modelBuilder.Entity("ContosoUniversity.Models.Instructor", b =>
-                {
-                    b.Property<int>("ID")
-                        .ValueGeneratedOnAdd();
-
-                    b.Property<string>("FirstMidName")
-                        .IsRequired()
-                        .HasColumnName("FirstName")
-                        .HasMaxLength(50);
-
-                    b.Property<DateTime?>("HireDate");
-
-                    b.Property<string>("LastName")
-                        .IsRequired()
-                        .HasMaxLength(50);
-
-                    b.HasKey("ID");
-
-                    b.ToTable("Instructor");
-                });
-
             modelBuilder.Entity("ContosoUniversity.Models.OfficeAssignment", b =>
                 {
                     b.Property<int>("InstructorID");
@@ -131,12 +111,13 @@ namespace ContosoUniversity.Migrations
                     b.ToTable("OfficeAssignment");
                 });
 
-            modelBuilder.Entity("ContosoUniversity.Models.Student", b =>
+            modelBuilder.Entity("ContosoUniversity.Models.Person", b =>
                 {
                     b.Property<int>("ID")
                         .ValueGeneratedOnAdd();
 
-                    b.Property<DateTime>("EnrollmentDate");
+                    b.Property<string>("Discriminator")
+                        .IsRequired();
 
                     b.Property<string>("FirstMidName")
                         .IsRequired()
@@ -144,11 +125,36 @@ namespace ContosoUniversity.Migrations
                         .HasMaxLength(50);
 
                     b.Property<string>("LastName")
+                        .IsRequired()
                         .HasMaxLength(50);
 
                     b.HasKey("ID");
 
+                    b.ToTable("Person");
+
+                    b.HasDiscriminator<string>("Discriminator").HasValue("Person");
+                });
+
+            modelBuilder.Entity("ContosoUniversity.Models.Instructor", b =>
+                {
+                    b.HasBaseType("ContosoUniversity.Models.Person");
+
+                    b.Property<DateTime?>("HireDate");
+
+                    b.ToTable("Instructor");
+
+                    b.HasDiscriminator().HasValue("Instructor");
+                });
+
+            modelBuilder.Entity("ContosoUniversity.Models.Student", b =>
+                {
+                    b.HasBaseType("ContosoUniversity.Models.Person");
+
+                    b.Property<DateTime>("EnrollmentDate");
+
                     b.ToTable("Student");
+
+                    b.HasDiscriminator().HasValue("Student");
                 });
 
             modelBuilder.Entity("ContosoUniversity.Models.Course", b =>
--- Models/Student.cs
+++ Models/Student.cs
@@ -5,28 +5,14 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
-    public class Student
+    public class Student:Person
     {
-        public int ID {get;set;}
- 
-        [StringLength(50,MinimumLength=1)]
-        [Display(Name="Last Name")]
-        public string LastName {get;set;}
-        [Required]
-        [StringLength(50, ErrorMessage = "First Name cannot longer than 50 chars")]
-        [Display(Name = "First Name")]
-        [Column("FirstName")]
-        public string FirstMidName {get; set;}
+    
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}",ApplyFormatInEditMode=true)]
         [Display(Name = "Enrollment Date")]
         public  DateTime EnrollmentDate {get; set;}
-        public string FullName 
-        {
-            get {
-                return LastName + "," + FirstMidName;
-            }
-        }
+
         public ICollection<Enrollment> Enrollments {get; set;}
     }
 }
